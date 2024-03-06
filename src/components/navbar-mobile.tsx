fa5344769e51616594b4646401c67d1cd5414178
Konstantin Lindner <hello@konte.se>
2024-03-05 22:51:33 -0800

    fix mobile nav

--- src/components/navbar-mobile.tsx
+++ src/components/navbar-mobile.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { MouseEvent, MutableRefObject, useState } from 'react'
 
 import Link from 'next/link'
 
@@ -6,23 +6,23 @@ import { useLockBody } from '@/hooks/use-lock-body'
 
 import { cn } from '@/lib/utils'
 
+import { useClickAway } from '@uidotdev/usehooks'
+
 import { Menu, X } from 'lucide-react'
 
 import { NavbarProps } from '@/components/navbar'
 import ThemeToggle from '@/components/theme-toggle'
-import { Button } from '@/components/ui/button'
 
 export default function NavbarMobile({ items, currentPage }: NavbarProps) {
 	const [menuOpen, setMenuOpen] = useState<boolean>(false)
 	useLockBody(menuOpen)
 
-	// TODO handleOverlayClick
-	const handleOverlayClick = () => {
+	const ref = useClickAway(() => {
 		setMenuOpen(false)
-	}
+	}) as MutableRefObject<HTMLDivElement | null>
 
-	const handleMenuClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.stopPropagation()
+	const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation()
 		setMenuOpen(!menuOpen)
 	}
 
@@ -32,7 +32,12 @@ export default function NavbarMobile({ items, currentPage }: NavbarProps) {
 
 	return (
 		<header className="relative flex h-24 items-center justify-between px-10">
-			<button onClick={handleMenuClick} className="relative z-10">
+			<button
+				onClick={handleMenuClick}
+				aria-label="Toggle menu"
+				aria-pressed={menuOpen}
+				className="relative z-10"
+			>
 				{menuOpen ?
 					<X className="size-8" />
 				:	<Menu className="size-8" />}
@@ -41,27 +46,27 @@ export default function NavbarMobile({ items, currentPage }: NavbarProps) {
 			<ThemeToggle isMobile />
 
 			{menuOpen && (
-				<>
-					<div className="fixed inset-0 top-72 z-10 bg-secondary opacity-80"></div>
-					<nav className="fixed inset-0 top-24 z-30 grid grid-flow-row auto-rows-max overflow-auto shadow-md animate-in slide-in-from-top-3">
-						<div className="grid justify-center gap-10 bg-background px-20 pb-20 pt-5 text-center shadow-md">
-							{items.map((item, index) => (
-								<Link
-									key={index}
-									href={item.href}
-									className={cn(
-										'text-lg',
-										currentPage === item.activeRoute &&
-											'font-bold',
-									)}
-									onClick={handleLinkClick}
-								>
-									{item.title}
-								</Link>
-							))}
-						</div>
-					</nav>
-				</>
+				<nav className="fixed inset-0 top-24 z-30 grid grid-flow-row auto-rows-max overflow-auto shadow-lg animate-in slide-in-from-top-3">
+					<div
+						ref={ref}
+						className="grid justify-center gap-10 bg-background px-20 pb-20 pt-5 text-center shadow-md"
+					>
+						{items.map((item, index) => (
+							<Link
+								key={index}
+								href={item.href}
+								className={cn(
+									'text-lg',
+									currentPage === item.activeRoute &&
+										'font-bold',
+								)}
+								onClick={handleLinkClick}
+							>
+								{item.title}
+							</Link>
+						))}
+					</div>
+				</nav>
 			)}
 		</header>
 	)
