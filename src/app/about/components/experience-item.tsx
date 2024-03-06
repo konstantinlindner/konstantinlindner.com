86111544e977f7f3a9b061caada85c053dc571ea
Konstantin Lindner <hello@konte.se>
2024-03-05 21:59:50 -0800

    create experience item

---
+++ src/app/about/components/experience-item.tsx
@@ -0,0 +1,33 @@
+type ExperienceItemProps = {
+	header: string
+	description: string
+	date: string
+	text: string
+	text2?: string
+	text3?: string
+}
+
+export default function ExperienceItem({
+	header,
+	description,
+	date,
+	text,
+	text2,
+	text3,
+}: ExperienceItemProps) {
+	return (
+		<div className="space-y-5">
+			<div>
+				<h3 className="text-lg font-bold">{header}</h3>
+				<h4 className="text-lg italic">{description}</h4>
+				<p className="text-sm">{date}</p>
+			</div>
+
+			<div className="space-y-4">
+				<p>{text}</p>
+				{text2 && <p>{text2}</p>}
+				{text3 && <p>{text3}</p>}
+			</div>
+		</div>
+	)
+}
